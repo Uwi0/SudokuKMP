7fbb8158b1082e10a3172dd4595c2f58cbc12efc
uwi0 <kakapoweb@gmail.com>
2024-10-03 05:40:44 +0700

    changer ios minimum version and creation action button view

---
+++ iosApp/iosApp/screen/board/component/controller/ActionButtonView.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+import Shared
+
+struct ActionButtonView: View {
+    let action: GameAction
+    let onClick: () -> Void
+    
+    private var resource: (image: String, text: String) { action.asResource() }
+    
+    var body: some View {
+        Button(
+            action: onClick, label: {
+                VStack(spacing: 4) {
+                    Image(systemName: resource.image)
+                        .fontWeight(.bold)
+                    Text(resource.text)
+                }
+                .font(.subheadline)
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            }
+        )
+    }
+}
+
+extension GameAction {
+    func asResource() -> (String, String) {
+        switch self {
+            case .erase: return ("eraser.line.dashed", "Erase")
+            case .hint: return ("lightbulb.max", "Hint")
+            case .note: return ("pencil.line", "Note")
+            case .undo: return ("arrow.uturn.backward", "Undo")
+            default: return ("", "")
+        }
+    }
+}
+
+#Preview {
+    ActionButtonView(action: GameAction.erase, onClick: {})
+}
