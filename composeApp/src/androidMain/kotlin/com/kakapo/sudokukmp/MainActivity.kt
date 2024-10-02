6bb7073832882b055fc7fda13a37d61c572924a4
uwi0 <kakapoweb@gmail.com>
2024-10-02 11:08:01 +0700

    Creating Base Board using Canvas and drawLine

--- composeApp/src/androidMain/kotlin/com/kakapo/sudokukmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/kakapo/sudokukmp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kakapo.sudokukmp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.kakapo.sudokukmp.board.BoardGameView
@@ -11,7 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+
         }
     }
 }
@@ -19,5 +20,7 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    BoardGameView()
+    Surface {
+        BoardGameView(size = 9, board = emptyList())
+    }
 }
--- composeApp/src/commonMain/kotlin/com/kakapo/sudokukmp/board/BoarGameView.kt
+++ composeApp/src/commonMain/kotlin/com/kakapo/sudokukmp/board/BoarGameView.kt
@@ -1,26 +1,65 @@
 package com.kakapo.sudokukmp.board
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Text
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import com.kakapo.sudokukmp.model.Cage
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import com.kakapo.sudokukmp.model.Cell
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
 
 @Composable
 fun BoardGameView(
-    cage: Cage = Cage(cells = listOf(Cell(row = 9, col = 9)))
-){
-    if (cage.cells.isNotEmpty()){
-        Column {
-            for(row in 0 until cage.cells.first().col) {
-                Row {
-                    for (col in 0 until cage.cells.first().row) {
-                        Text(text = "X")
-                    }
+    size: Int = 81,
+    board: List<List<Cell>>,
+    enabled: Boolean = true
+) {
+    val vertThick by remember(size) { mutableIntStateOf(floor(sqrt(size.toFloat())).toInt()) }
+    val horThick by remember(size) { mutableIntStateOf(ceil(sqrt(size.toFloat())).toInt()) }
+    val thickLineColor = MaterialTheme.colorScheme.primary
+    val thinLineColor = MaterialTheme.colorScheme.error
+    val thinLineWidth = with(LocalDensity.current) { 1.3.dp.toPx() }
+    val thickLineWidth = with(LocalDensity.current) { 1.3.dp.toPx() }
+    BoxWithConstraints(modifier = Modifier.fillMaxWidth().aspectRatio(1f).padding(4.dp)) {
+        val maxWidth = constraints.maxWidth.toFloat()
+        val cellSize = maxWidth / size.toFloat()
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            for (i in 1 until size) {
+                val isThickLine = i % horThick == 0
+                drawLine(
+                    color = if (isThickLine) thickLineColor else thinLineColor,
+                    start = Offset(cellSize * i.toFloat(), 0f),
+                    end = Offset(cellSize * i.toFloat(), maxWidth),
+                    strokeWidth = if (isThickLine) thickLineWidth else thinLineWidth
+                )
+            }
+
+            for (i in 1 until size) {
+                val isThickLine = i % vertThick == 0
+                if (maxWidth >= cellSize * i) {
+                    drawLine(
+                        color = if (isThickLine) thickLineColor else thinLineColor,
+                        start = Offset(0f, cellSize * i.toFloat()),
+                        end = Offset(maxWidth, cellSize * i.toFloat()),
+                        strokeWidth = if (isThickLine) thickLineWidth else thinLineWidth
+                    )
                 }
             }
         }
     }
-
 }
