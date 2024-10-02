641f85329b146330682c94fe3a12619fa4ffc626
uwi0 <kakapoweb@gmail.com>
2024-10-03 03:19:47 +0700

    finishing general structure ui for BoardScreen

---
+++ composeApp/src/commonMain/kotlin/com/kakapo/sudokukmp/component/topBoard/TopBoardView.kt
@@ -0,0 +1,59 @@
+package com.kakapo.sudokukmp.component.topBoard
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+internal fun TopBoardView() {
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+        DifficultyTextView()
+        TimerTextView()
+        PauseButton()
+    }
+}
+
+@Composable
+private fun DifficultyTextView() {
+    Column(
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        Text(text = "Difficulty", style = MaterialTheme.typography.titleMedium)
+        Text(text = "Easy", style = MaterialTheme.typography.bodyMedium)
+    }
+}
+
+@Composable
+private fun TimerTextView() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ){
+        Text(text = "Timer", style = MaterialTheme.typography.titleMedium)
+        Text(text = "00:00", style = MaterialTheme.typography.bodyMedium)
+    }
+}
+
+@Composable
+private fun PauseButton() {
+    Surface(shape = CircleShape, color = MaterialTheme.colorScheme.surfaceVariant) {
+        IconButton(onClick = {}) {
+            Icon(imageVector = Icons.Default.Pause, contentDescription = null)
+        }
+    }
+
+}
--- composeApp/src/commonMain/kotlin/com/kakapo/sudokukmp/screen/board/BoardScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kakapo/sudokukmp/screen/board/BoardScreen.kt
@@ -3,13 +3,14 @@ package com.kakapo.sudokukmp.screen.board
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.kakapo.sudokukmp.component.board.BoardGameView
 import com.kakapo.sudokukmp.component.controller.GameControllerView
-import com.kakapo.sudokukmp.component.controller.GameKeyboardView
+import com.kakapo.sudokukmp.component.topBoard.TopBoardView
 import com.kakapo.sudokukmp.presentation.game.GameUiState
 
 @Composable
@@ -26,6 +27,8 @@ internal fun BoardScreen() {
                     .padding(paddingValues)
                     .padding(vertical = 24.dp, horizontal = 16.dp)
             ) {
+                TopBoardView()
+                Spacer(Modifier.size(24.dp))
                 BoardGameView(boards = emptyList())
                 Spacer(Modifier.weight(1f))
                 GameControllerView(uiState = GameUiState(size = 9))
