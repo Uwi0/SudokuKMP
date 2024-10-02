5e601c955b5dbe50e9ea254eb669e484abc50a7b
uwi0 <kakapoweb@gmail.com>
2024-10-03 05:14:06 +0700

    creating ui GameKeyaboardView

---
+++ iosApp/iosApp/screen/board/component/controller/GameKeyboardView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct GameKeyboardView: View {
+    
+    let numbers: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    
+    var body: some View {
+        HStack {
+            ForEach(numbers, id: \.self) { number in
+                KeyboardItemView(number: number, remainingUse: 4)
+            }
+        }
+    }
+}
+
+#Preview {
+    GameKeyboardView()
+}
